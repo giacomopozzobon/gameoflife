a29a5f12be610786c62aa9d897ad0e9f49c7514f
Giacomo Pozzobon <g97.pozzobon@gmail.com>
2022-07-08 02:30:26 +0200

    Upload & Read a .txt File

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,30 @@
 class HomeController < ApplicationController
+  @@lines = Array.new
+
   def index
   end
+
+  def game
+      @lines = @@lines
+  end
+
+  def uploadfile
+    if params[:file].present?
+      
+      filename = params[:file]
+
+      if filename.respond_to?(:read)
+        @@lines = filename.read.split("\r\n")
+      elsif filename.respond_to?(:path)
+        @@lines = File.read(filename.path).split("\r\n")
+      else
+        logger.error "Bad file_data: #{filename.class.name}: #    
+        {@filename.inspect}"
+      end
+
+      redirect_to home_game_path
+
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+  post 'home/uploadfile'
+  get 'home/game'
 
   #get 'home/index'
 end
